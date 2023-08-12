b29349c1550591154a469211b85e721ecb9a3149
Ray Jeong <devops3199@gmail.com>
2023-08-12 23:33:20 +0900

    [feat] exception aspect

---
+++ src/main/java/com/erp/hr/controller/ExceptionController.java
@@ -0,0 +1,33 @@
+package com.erp.hr.controller;
+
+import com.erp.hr.model.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class)
+public class ExceptionController extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        Response response = new Response(statusCode.toString(),
+                ex.getBindingResult().toString());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Response> handleException(Exception exception) {
+        Response errorResponse = new Response("500", exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
