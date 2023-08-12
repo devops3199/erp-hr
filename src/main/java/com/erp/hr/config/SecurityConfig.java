e7e8d57ae3ef9dc5b13f2ae9c79038a5d049f3a9
Ray Jeong <devops3199@gmail.com>
2023-08-12 22:51:46 +0900

    [refactor] rename

--- src/main/java/com/erp/hr/config/SecurityConfig.java
+++ src/main/java/com/erp/hr/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +12,7 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req.requestMatchers("/api/**").permitAll())
                 .httpBasic(Customizer.withDefaults());
 
