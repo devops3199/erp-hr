cff899f111e7b7509812c75e8b91fa73c56e8978
Ray Jeong <devops3199@gmail.com>
2023-08-19 06:53:00 +0900

    [feat] check auth

--- src/main/java/com/erp/hr/auth/service/AuthService.java
+++ src/main/java/com/erp/hr/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.erp.hr.auth.service;
 
 import com.erp.hr.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
--- src/main/java/com/erp/hr/config/SecurityConfig.java
+++ src/main/java/com/erp/hr/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.erp.hr.config;
 
+import com.erp.hr.auth.filter.AuthFilter;
 import com.erp.hr.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,21 +17,32 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
 
     private final EmployeeRepository employeeRepository;
+    private final AuthFilter authFilter;
 
     @Autowired
-    public SecurityConfig(EmployeeRepository employeeRepository) {
+    public SecurityConfig(EmployeeRepository employeeRepository, AuthFilter authFilter) {
         this.employeeRepository = employeeRepository;
+        this.authFilter = authFilter;
     }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers("/api/**").permitAll())
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/api/login").permitAll()
+                        .requestMatchers("/api/employee/**").hasAnyAuthority("Admin", "Leader", "Staff")
+                        .anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(this.authenticationProvider())
+                .addFilterBefore(this.authFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
